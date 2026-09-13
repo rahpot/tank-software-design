c14dc715f4aedf8d56e2d8f1046b8d29b40c7273
rahpot <micor20052004@gmail.com>
2026-09-13 20:13:15 +0300

    add default classes

--- src/main/java/ru/mipt/bit/platformer/TextureClass.java
+++ src/main/java/ru/mipt/bit/platformer/TextureClass.java
@@ -15,6 +15,8 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Shape2D;
+
 import ru.mipt.bit.platformer.util.TileMovement;
 
 import static com.badlogic.gdx.Input.Keys.*;
@@ -22,30 +24,138 @@ import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 import static com.badlogic.gdx.math.MathUtils.isEqual;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.*;
 
+// Texture class
+public class TextureClass {
+    private Texture image;
+    private TextureRegion region;
+
+    public TextureClass(Texture image) {
+        this.image = image;
+        this.region = new TextureRegion(image);
+    }
+
+    public TextureClass(Texture image, TextureRegion region) {
+        this.image = image;
+        this.region = region;
+    }
+}
+
+public enum Direction {
+    UP(0, 1, 90f),
+    DOWN(0, -1, 270f),
+    LEFT(-1, 0, 180f),
+    RIGHT(1, 0, 0f);
+
+    private final int deltaX;
+    private final int deltaY;
+    private final float rotationAngle;
+
+    Direction(int deltaX, int deltaY, float rotationAngle) {
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+        this.rotationAngle = rotationAngle;
+    }
+
+    public GridPoint2 apply(GridPoint2 from) {
+        return new GridPoint2(from.x + deltaX, from.y + deltaY);
+    }
+
+    public float getRotationAngle() {
+        return rotationAngle;
+    }
+}
+
+
+// Родительский класс для описания всех игровых объектов
+public class GameObject {
+    private Shape2D collisionShape;
+    private GridPoint2 position;
+    private float rotation;
+    private TextureClass objTexture;
+
+    public GridPoint2 getCurrentPosition () {
+        return this.position;
+    }
+
+    public float getCurrentRotation () {
+        return this.rotation;
+    } 
+
+    public void turn(Direction finishDirection) {
+        this.rotation = finishDirection.getRotationAngle();
+    }
+
+    public GameObject(Shape2D collisionShape, GridPoint2 position, float rotation, TextureClass texture) {
+        this.collisionShape = collisionShape;
+        this.position = position;
+        this.rotation = rotation;
+        this.objTexture = texture;
+    }
+
+    public GameObject(Shape2D collisionShape, TextureClass texture) {
+        this(collisionShape, new GridPoint2(0, 0), 0.0F, texture);
+    }
+
+    public GameObject(Shape2D collisionShape, GridPoint2 position, TextureClass texture) {
+        this(collisionShape, position, 0.0F, texture);
+    }
+
+    public GameObject(Shape2D collisionShape, float rotation, TextureClass texture) {
+        this(collisionShape, new GridPoint2(0, 0), rotation, texture);
+    }
+}
+
+public class PlayerObject extends GameObject {
+    private GridPoint2 playerDestinationCoordinates;
+    private boolean playerMovementProgress;
+    
+    void move () {
+        if (playerMovementProgress) {
+            // check potential player destination for collision with obstacles
+            if (!treeObstacleCoordinates.equals(incrementedY(getPosition()))) {
+                playerDestinationCoordinates.y++;
+                playerMovementProgress = false;
+            }
+            this.turn(Direction.RIGHT);
+        }
+    }
+
+    PlayerObject(Shape2D collisionShape,GridPoint2 position, TextureClass texture) {
+        super(collisionShape, position, texture);      
+        this.playerDestinationCoordinates = new GridPoint2(position); 
+    }
+}
+
+
+
+
+
 public class GameDesktopLauncher implements ApplicationListener {
-
-    private static final float MOVEMENT_SPEED = 0.4f;
-
+    // Переменная скапливающая команды рисования и отрисовывающая их за один тик
     private Batch batch;
+    // Всё для графики (изображения классы с положением и тд)
+    private Texture blueTankTexture; // Изображение танка из tank_blue.png
+    private TextureRegion playerGraphics; // Модифицированное изображение танка, которое отрисовывается(скорее всего убран фон) 
+    private Texture greenTreeTexture; // Изображение дерева из greenTree.png
+    private TextureRegion treeObstacleGraphics; // Модифицированное изображение дерева, которое отрисовывается
 
+    // Работа с картой 
     private TiledMap level;
     private MapRenderer levelRenderer;
     private TileMovement tileMovement;
 
-    private Texture blueTankTexture;
-    private TextureRegion playerGraphics;
-    private Rectangle playerRectangle;
+    // Скорость танка
+    private static final float MOVEMENT_SPEED = 0.4f;
+    private Rectangle playerRectangle; // можно сказать колайдер танка
     // player current position coordinates on level 10x8 grid (e.g. x=0, y=1)
-    private GridPoint2 playerCoordinates;
+    private GridPoint2 playerCoordinates; // положение танка (напрямую соотноится с playerRectangle)
     // which tile the player want to go next
-    private GridPoint2 playerDestinationCoordinates;
-    private float playerMovementProgress = 1f;
-    private float playerRotation;
+    private GridPoint2 playerDestinationCoordinates; // нынешняя цель танка, пока не достигнет не обновляется
+    private float playerMovementProgress = 1f; // Отслеживает прогресс передвижения танка от 0 до 1, когда закончил можно переходить к следующей клетке 
+    private float playerRotation; // Угол поворота танка
 
-    private Texture greenTreeTexture;
-    private TextureRegion treeObstacleGraphics;
-    private GridPoint2 treeObstacleCoordinates = new GridPoint2();
-    private Rectangle treeObstacleRectangle = new Rectangle();
+    private GridPoint2 treeObstacleCoordinates = new GridPoint2(); // Клетка на сетке, где стоит дерево
+    private Rectangle treeObstacleRectangle = new Rectangle(); // колайдер дерева
 
     @Override
     public void create() {
@@ -61,7 +171,7 @@ public class GameDesktopLauncher implements ApplicationListener {
         blueTankTexture = new Texture("images/tank_blue.png");
         // TextureRegion represents Texture portion, there may be many TextureRegion instances of the same Texture
         playerGraphics = new TextureRegion(blueTankTexture);
-        playerRectangle = createBoundingRectangle(playerGraphics);
+        playerRectangle = createBoundingRectangle(playerGraphics); // Отвечает за создание колайдера
         // set player initial position
         playerDestinationCoordinates = new GridPoint2(1, 1);
         playerCoordinates = new GridPoint2(playerDestinationCoordinates);
@@ -84,14 +194,7 @@ public class GameDesktopLauncher implements ApplicationListener {
         float deltaTime = Gdx.graphics.getDeltaTime();
 
         if (Gdx.input.isKeyPressed(UP) || Gdx.input.isKeyPressed(W)) {
-            if (isEqual(playerMovementProgress, 1f)) {
-                // check potential player destination for collision with obstacles
-                if (!treeObstacleCoordinates.equals(incrementedY(playerCoordinates))) {
-                    playerDestinationCoordinates.y++;
-                    playerMovementProgress = 0f;
-                }
-                playerRotation = 90f;
-            }
+            // Здесь должен быть move (UP)
         }
         if (Gdx.input.isKeyPressed(LEFT) || Gdx.input.isKeyPressed(A)) {
             if (isEqual(playerMovementProgress, 1f)) {
